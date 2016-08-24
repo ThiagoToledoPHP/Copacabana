874e4e3e83745b0e11b4a0a9c8b749fff27c1dc0
thiagotoledo <javaephp@gmail.com>
2016-08-24 01:16:24 -0300

    test

--- tests/selenium/CopacabanaTest.php
+++ tests/selenium/CopacabanaTest.php
@@ -22,16 +22,24 @@ class LoginTest extends PHPUnit_Extensions_Selenium2TestCase
     {
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://www.example.com/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('http://www.example.com/');
         $this->assertEquals('Example Domain', $this->title());
     }
+
 
     /**
      * Simple test title
      */
-    public function testTitle()
+    /*
+    public function testTitle2()
     {
         $this->url(URL_SELENIUM_ACCESS);
         $this->assertEquals(':)', $this->byId('smile')->text());
     }
+    */
 
 }
