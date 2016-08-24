9de5f829102c4b1d30c62a33c0e13b52504763fe
thiagotoledo <javaephp@gmail.com>
2016-08-24 12:45:47 -0300

    test

--- tests/selenium/CopacabanaTest.php
+++ tests/selenium/CopacabanaTest.php
@@ -20,6 +20,7 @@ class LoginTest extends PHPUnit_Extensions_Selenium2TestCase
      */
     protected function setUp()
     {
+        $this->setPort(8000);
         $this->setBrowser('firefox');
         $this->setBrowserUrl('http://www.example.com/');
     }
